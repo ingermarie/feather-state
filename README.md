1ddebde25bedf9ce131b8f8519fdf615c3d34749
Inger-Marie Nilsen <ingermarien@gmail.com>
2023-12-28 19:47:34 +0100

    Version 1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,139 @@
-# feather-state
+# Feather State
+![gzip](https://img.shields.io/badge/gzip-351_bytes-green)
+![license](https://img.shields.io/badge/license-ISC-blue)
+![version](https://img.shields.io/badge/npm-v1.0.0-blue)
+
+✨ A feather light state framework ✨ 351 bytes minified and gzipped - no dependencies
+
+Live examples:
+- [Feather To-Do app](https://codesandbox.io/p/devbox/feather-to-do-app-k5ss8j)
+
+Companion frameworks:
+- Render - [feather-render](https://www.npmjs.com/package/feather-render)
+- State React - [feather-state-react](https://www.npmjs.com/package/feather-state-react)
+
+[![version](https://img.shields.io/badge/Buy_me_a_coffee_❤️-724e2c)](https://www.paypal.com/paypalme/featherframework)
+
+## Getting started
+```
+npm i feather-state
+```
+
+## Usage
+```typescript
+import { store } from 'feather-state';
+
+const { watch, ...state } = store({
+  completeCount: 1,
+  todos: [{
+    title: 'Walk dog',
+    done: true
+  }, {
+    title: 'Dinner',
+    done: false
+  }]
+});
+
+watch(state, 'todos', (next, prev) => {
+  console.log(next, prev);
+});
+
+function addTodo(title: string): void {
+  const newTodo = {
+    title: title,
+    done: false
+  };
+  state.todos = [
+    ...todoStore.todos,
+    newTodo
+  ];
+};
+```
+
+## Documentation
+See [example](##Examples) usage below.
+
+### `store()`
+```typescript
+store(state) => { state, watch() } | { ...state, watch() };
+```
+#### Parameters
+- `state`: state value
+
+#### Return values
+- `state` | `...state`: state value
+- `watch()`: watch for shallow mutations
+
+---
+
+### `store() => watch()`
+```typescript
+watch(parent, key, callback) => unwatch();
+```
+#### Parameters
+- `parent`: parent object of watched value
+- `key`: key of watched value
+- `callback()`: function called when value changes
+
+#### Return value
+- `unwatch()`: function to unwatch value
+
+## Examples
+### Object values: e.g. `array`, `object`
+Properties are assigned directly to the store object:
+
+```typescript
+const { watch, ...state } = store({
+  isPlaying: true,
+  playPauseAriaLabel: 'Pause'
+});
+
+watch(state, 'isPlaying', (next) => {
+  if (next) {
+    videoEl.play();
+    playPauseAriaLabel = 'Pause';
+  } else {
+    videoEl.pause();
+    playPauseAriaLabel = 'Play';
+  }
+});
+
+const handleVisibilitychange = () => {
+  if (document.visibilityState === "visible") {
+    video.isPlaying = true;
+  } else {
+    video.isPlaying = false;
+  }
+};
+```
+
+### Primitive values: e.g. `string`, `number`, `boolean`
+Values get stored in the `state` property:
+
+```typescript
+const { watch, ...isPlaying } = store(true);
+const playPauseAriaLabel = store('Pause');
+
+watch(isPlaying, 'state', (next) => {
+  if (next) {
+    videoEl.play();
+    playPauseAriaLabel.state = 'Pause';
+  } else {
+    videoEl.pause();
+    playPauseAriaLabel.state = 'Play';
+  }
+});
+
+const handleVisibilitychange = () => {
+  if (document.visibilityState === "visible") {
+    isPlaying.state = true;
+  } else {
+    isPlaying.state = false;
+  }
+};
+```
+
+## Roadmap 🚀
+- Minified version via CDN
+- Cleaner way of referencing values in watcher parameters
+- Get even tinier in size
--- src/store.ts
+++ src/store.ts
@@ -1,80 +1,80 @@
-type Store<S = unknown> = {
+type IStore<S = unknown> = Store<S> & {
 	watchers: WeakMap<
 		object,
 		Record<PropertyKey, ((next: any, prev: any) => void)[]>
 	>;
-	watch: <R extends object, K extends keyof R>(
-		ref: R,
+};
+
+export type Store<S = unknown> = {
+	watch: <P extends object, K extends keyof Omit<P, 'watch'>>(
+		parent: P,
 		key: K,
-		notify: (next: R[K], prev: R[K]) => void
+		notify: (next: P[K], prev: P[K]) => void
 	) => () => void;
 } & (S extends object ? S : { state: S });
 
-function store<S>(state: S): Store<S> {
-	const watchers: Store['watchers'] = new WeakMap();
-
-	const watch: Store['watch'] = (ref, key, notify) => {
-		let internalValue = ref[key];
-		Object.defineProperty(ref, key, {
-			set: (value) => {
-				const refWatchers = watchers.get(ref);
-				const keyWatchers = refWatchers?.[key];
-				const previousValue = internalValue;
-				if (value === previousValue) return;
-
-				internalValue = value;
-				keyWatchers?.forEach((w) => w(value, previousValue));
-			},
-			get: () => {
-				return internalValue;
-			}
-		});
-
-		// Watch
-		let refWatchers = watchers.get(ref);
-		let keyWatchers = refWatchers?.[key];
-
-		if (!refWatchers) {
-			refWatchers = {};
-			watchers.set(ref, refWatchers);
-		}
-		if (!keyWatchers) {
-			keyWatchers = [];
-			refWatchers[key] = keyWatchers;
-		}
-		const length = keyWatchers.push(notify);
-		const index = length - 1;
-
-		// Unwatch
-		return () => {
-			const refWatchers = watchers.get(ref);
+function watch<P extends object, K extends keyof Omit<P, 'watch'>>(
+	this: IStore,
+	parent: P,
+	key: K,
+	notify: (next: P[K], prev: P[K]) => void
+): () => void {
+	let internalValue = parent[key];
+	Object.defineProperty(parent, key, {
+		set: (value) => {
+			const refWatchers = this.watchers.get(parent);
 			const keyWatchers = refWatchers?.[key];
-			if (!refWatchers) return;
+			const previousValue = internalValue;
+			if (value === previousValue) return;
 
-			keyWatchers?.splice(index, 1);
+			internalValue = value;
+			keyWatchers?.forEach((w) => w(value, previousValue));
+		},
+		get: () => {
+			return internalValue;
+		}
+	});
 
-			if (!keyWatchers?.length) {
-				delete refWatchers[key];
-			}
-			if (!Object.keys(refWatchers).length) {
-				watchers.delete(ref);
-			}
-		};
+	// Watch
+	let refWatchers = this.watchers.get(parent);
+	let keyWatchers = refWatchers?.[key];
+
+	if (!refWatchers) {
+		refWatchers = {};
+		this.watchers.set(parent, refWatchers);
+	}
+	if (!keyWatchers) {
+		keyWatchers = [];
+		refWatchers[key] = keyWatchers;
+	}
+	const length = keyWatchers.push(notify);
+	const index = length - 1;
+
+	// Unwatch
+	return () => {
+		const refWatchers = this.watchers.get(parent);
+		const keyWatchers = refWatchers?.[key];
+		if (!refWatchers) return;
+
+		keyWatchers?.splice(index, 1);
+
+		if (!keyWatchers?.length) {
+			delete refWatchers[key];
+		}
+		if (!Object.keys(refWatchers).length) {
+			this.watchers.delete(parent);
+		}
 	};
+};
 
-	const storeApi = {} as Store<S>;
+export const store = <S>(state: S): Store<S> => {
+	const watchers: IStore['watchers'] = new WeakMap();
+	const storeApi = state !== null && typeof state === 'object' ? state : { state };
 
 	Object.defineProperties(storeApi, {
 		watchers: { value: watchers },
-		watch: { value: watch },
+		watch: { value: watch.bind(storeApi as IStore) }
 	});
 
-	if (state !== null && typeof state === 'object') {
-		Object.assign(storeApi, state);
-	} else {
-		Object.assign(storeApi, { state });
-	}
-	return storeApi;
-}
-
-export { Store, store };
+	return storeApi as Store<S>;
+};
